f0929c42df0f504b8a094491453dc90caa1c1fb1
JTL <rakmial@gmail.com>
2016-06-30 11:08:59 -0700

    V2 Update

--- Code/tagexplore.py
+++ Code/tagexplore.py
@@ -5,17 +5,17 @@ import re
 
 lower = re.compile(r'^([a-z]|_)*$')
 lower_colon = re.compile(r'^([a-z]|_)*:([a-z]|_)*$')
-problemchars = re.compile(r'[=\+/&<>;\'"\?%#$@\,\. \t\r\n]')
+problem_chars = re.compile(r'[=\+/&<>;\'"\?%#$@\,\. \t\r\n]')
 
 
 def key_type(element, keys):
     if element.tag == "tag":
-        discountcaps = element.attrib['k'].lower()
-        if problemchars.search(discountcaps):
-            keys['problemchars'] += 1
-        elif lower_colon.search(discountcaps):
+        discount_caps = element.attrib['k'].lower()
+        if problem_chars.search(discount_caps):
+            keys['problem_chars'] += 1
+        elif lower_colon.search(discount_caps):
             keys['lower_colon'] += 1
-        elif lower.search(discountcaps):
+        elif lower.search(discount_caps):
             keys['lower'] += 1
         else:
             keys['other'] += 1
@@ -24,26 +24,26 @@ def key_type(element, keys):
 
 def other_keys(element):                #prints 'other' keys, discounts all-caps 'others'
     if element.tag == "tag":
-        discountcaps = element.attrib['k'].lower()
-        if not problemchars.search(discountcaps):
-            if not lower_colon.search(discountcaps):
-                if not lower.search(discountcaps):
-                    return discountcaps
+        discount_caps = element.attrib['k'].lower()
+        if not problem_chars.search(discount_caps):
+            if not lower_colon.search(discount_caps):
+                if not lower.search(discount_caps):
+                    return discount_caps
 
 
 def process_map(filename):
-    keys = {"lower": 0, "lower_colon": 0, "problemchars": 0, "other": 0}
-    otherlist = []
-    otherdict = {}
+    keys = {"lower": 0, "lower_colon": 0, "problem_chars": 0, "other": 0}
+    other_list = []
+    other_dict = {}
     for _, element in ET.iterparse(filename):
         keys = key_type(element, keys)
-        otherlist.append(other_keys(element))
-    for other in otherlist:
-        if other in otherdict:
-            otherdict[other] += 1
+        other_list.append(other_keys(element))
+    for other in other_list:
+        if other in other_dict:
+            other_dict[other] += 1
         else:
-            otherdict[other] = 1
+            other_dict[other] = 1
 
-    return keys, otherdict
+    return keys, other_dict
 
 print process_map(r'C:\Users\Bash\Desktop\Udacity\2_Data_Analysis\P3\Project\sample.osm')
