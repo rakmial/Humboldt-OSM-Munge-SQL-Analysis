9377fe2b9ef11cab949fb6f9264052c59c7d023f
rakmial <rakmial@gmail.com>
2016-11-30 09:02:17 -0800

    Update to V5

--- Code/tagmaul.py
+++ Code/tagmaul.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 
 import xml.etree.cElementTree as ET
-
+import sys
+print (sys.version)
+from pprint import pprint
 
 mapping = { "St": "Street",
             "St.": "Street",
@@ -17,6 +19,8 @@ mapping = { "St": "Street",
             "Ctr": "Center"
             }
 
+OSM_FILE = r"/run/media/jtl/Seagate Backup Plus Drive/Udacity/2_Data_Analysis/P3/Project/ProjectRepo/sample.osm"
+OUTPUT_FILE = r"/run/media/jtl/Seagate Backup Plus Drive/Udacity/2_Data_Analysis/P3/Project/ProjectRepo/update_sample.osm"
 
 def update_name(name, mapping):     # used in updater
     newname = None
@@ -31,32 +35,41 @@ def update_name(name, mapping):     # used in updater
 
 def updater(osm_file):
     osmfile = open(osm_file,"r+")
-    treeparse = ET.iterparse(osm_file, events=("start",))
+    treeparse = ET.iterparse(osm_file, events=("start","end"))
+    _, root = next(treeparse)
     for event, elem in treeparse:
-        if elem.tag == "node" or elem.tag == "way":
+        if event == 'end' and (elem.tag in ["node", "way"] ):
             maybename = ""
             istiger = False
             for tag in elem.iter("tag"):
+                istiger = False
                 if tag.attrib['k'] == "addr:street":
                     if tag.attrib['v'] != update_name(tag.attrib['v'], mapping):
-                        print 'Before:', tag.attrib['v']
+                        print ('Before:', tag.attrib['v'])
                         tag.attrib['v'] = update_name(tag.attrib['v'], mapping)
-                        print 'After:', tag.attrib['v']
+                        print ('After:', tag.attrib['v'])
                 if tag.attrib['k'] == 'name':
                     maybename = tag.attrib['v']
                 if tag.attrib['k'] == "tiger:name_base":
                     istiger = True
-            if istiger:
-                for tag in elem.iter("tag"):
-                    if tag.attrib['k'] == "name":
-                        if tag.attrib['v'] != update_name(tag.attrib['v'], mapping):
-                            print 'Before:', tag.attrib['v']
-                            tag.attrib['v'] = update_name(tag.attrib['v'], mapping)
-                            print 'After:', tag.attrib['v']
+                if istiger:
+                    for tag in elem.iter("tag"):
+                        if tag.attrib['k'] == "name":
+                            if tag.attrib['v'] != update_name(tag.attrib['v'], mapping):
+                                print ('Before:', tag.attrib['v'])
+                                tag.attrib['v'] = update_name(tag.attrib['v'], mapping)
+                                print ('After:', tag.attrib['v'])
 
-        #osmfile.write(ET.tostring(elem))
+        yield elem
+        root.clear()
     osmfile.close()
 
-#need to rehandle tiger name
+with open(OUTPUT_FILE, 'w') as output:
+    output.write('<?xml version="1.0" encoding="UTF-8"?>\n')
+    output.write('<osm>\n  ')
 
-updater(r'C:\Users\Bash\Desktop\Udacity\2_Data_Analysis\P3\Project\humboldt_california.osm\humboldt_california.osm')
+    for i, element in enumerate(updater(OSM_FILE)):
+        print (ET.tostring(element, encoding='utf-8')[:10])
+        output.write(ET.tostring(element, encoding='utf-8'))
+
+    output.write('</osm>')
